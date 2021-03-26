b090dec24a80d7c41826a7919595f2c0044d30ed
T0mMarch <80701489+T0mMarch@users.noreply.github.com>
2021-03-26 12:18:16 +0100

    lift + cpx

--- engine_gr_38.py
+++ engine_gr_38.py
@@ -131,7 +131,21 @@ def lift_clod(order,ant_dico,clod_dico)
 	version
 	-------
 	Specification : Marchal Tom (v.1 26/02/21)
+	Implémentation : Marchal Tom (v.1 26/03/21)
 	"""
+	order_p1 = Player_order[0]
+	order_p2 = Player_order[1]
+	for order in order_p1:
+		if 'drop' in order:
+			ant = order[:4]
+			ant_x = ant[:1]
+			ant_y = ant[3:]
+			ant = (ant_x,ant_y)
+			for clod in clod_dico:
+				#check if there is a clod 
+				if clod_dico[clod][0] == ant_dico[ant][0] and clod_dico[clod][1] == ant_dico[ant][1]:
+					ant_dico[ant][clod] == True
+					
 def Fight(order,ant_dico): 
 	""" Check if the ant can attack the other one, attack if he can.
 
@@ -449,6 +463,41 @@ def data(".\cpx_file"):
 		nb_lines += 1
 	return ant_dico, anthill_dico, clod_dico
 
+def cpx_file():
+    """Create the file with all the data.
+	
+	Version 
+	-------
+	Spécification : Marchal Tom (v.1 26/03/21)
+	Implémentation : Marchal Tom (v.1 06/03/21)
+	"""
+    #create coordinate of the map
+    map_x = random.randint(20,60)
+    map_y = random.randint(20,60)
+    #create the coordinate of the anthills
+    anthill_blue= [random.randint(1,map_x),  random.randint(1,map_y)]
+    anthill_red= [random.randint(1,map_x),  random.randint(1,map_y)]
+    #create the number of clods
+    clods_number = random.randint(15,int(((map_x+map_y)/2)))
+    #create the coordinate of the clods
+    clods = []
+    for clod in range (0,clods_number+1):
+        clod = [random.randint(0,map_x),  random.randint(0,map_y)]
+        #verify if the coordinate != the anthills coordinates
+        while clod[0] == anthill_blue[0] or clod[0] == anthill_red[0] and clod[1] == anthill_blue[1] or clod[1] == anthill_red[1]:
+            clod = [random.randint(0,map_x),  random.randint(0,map_y)]
+        #append the weight of the clod
+        clod.append(random.randint(1,3))
+        clods.append(clod)
+
+    #create txt file
+    fh = open('.\data_game.txt', 'w')
+    fh.write('map : \n%d %d \n'%(map_x,map_y))
+    fh.write('anthills : \n%d %d \n%d %d \n'%(anthill_red[0],anthill_red[1],anthill_blue[0],anthill_blue[1]))
+    fh.write('clods : \n')
+    for clod in clods:
+        fh.write('%d %d %d \n'%(clod[0],clod[1],clod[2]))
+    fh.close()
 #Squelette de la fonction : 
 
 Play_game( ):
